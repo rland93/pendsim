b380a748fdae99b964f06f0ecc6a19effbdb76b8
ms <msutherland@fastmail.com>
2021-04-14 20:19:36 -0700

    progress on documentation

--- pendulum/controller2.py
+++ pendulum/controller2.py
@@ -5,25 +5,117 @@ import numpy as np
 from collections import defaultdict
 import pandas as pd
 import scipy.integrate
-import random
+import random, datetime
+from multiprocessing.dummy import Pool
 from matplotlib import pyplot as plt
 import math
 
+
+    'B' : np.array([
+            [0.0],
+            [1.0 / pend.M],
+            [0.0],
+            [-1.0 / (pend.l * pend.M)]
+        ])
+}
+
 class Pendulum(object):
+    '''Pendulum object
+
+    Contains pendulum dynamical model and model parameters. 
+    '''
     def __init__(self, M, m, l, g=9.81, cfric=0.04, initial_state=np.array([0,0,0,0])):
+        '''New pendulum object.
+
+        Parameters
+        ----------
+        M : float
+            Cart mass
+        m : float
+            Pendulum mass
+        l : float
+            Pendulum length
+        g : float, optional
+            Gravitational constant, by default 9.81
+        cfric : float, optional
+            Cart wheel viscous friction constant, by default 0.04
+        initial_state : ndarray of float, shape (4,), optional
+            Initial pendulum state: [x, xdot, theta, thetadot], by default np.array([0,0,0,0]) (up position)
+        '''
         self.M = M
         self.m = m 
         self.l = l
         self.g = g
         self.cfric = cfric
         self.y_0 = initial_state
+        # jacobians of the system
+        # linearized about the upward position x=[0,0,0,0]
+        self.jacA = np.array([
+            [0, 1.0, 0, 0],
+            [0, 0, -self.m * self.g / self.M, 0.0],
+            [0, 0, 0, 1.0],
+            [0, 0, self.g * (self.m * self.M) / (self.l * self.M), 0.0]
+        ])
+        self.jacB = np.array([
+            [0.0],
+            [1.0 / self.M],
+            [0.0],
+            [-1.0 / (self.l * self.M)]
+        ])
 
-    def system_dynamics(self, t, y):
-        xdd = (y[4] + self.m * self.g * np.sin(y[2]) * np.cos(y[2]) - self.m * self.l * y[3] * y[3] *  np.sin(y[2])) / (self.M + self.m - self.m * np.cos(y[2]) * np.cos(y[2]))
-        tdd = (xdd * np.cos(y[2]) + self.g * np.sin(y[2]) ) / self.l
-        return np.array([y[1] * (1 - self.cfric), xdd, y[3], tdd, y[4]])
+    def system_dynamics(self, t, state):
+        '''governing equation for pendulum system dynamics, given by:
+        
+        y' = f(t, y)
 
-    def solve(self, dt, system_state, u, solve_args):
+        See https://en.wikipedia.org/wiki/Inverted_pendulum for details
+
+        Parameters
+        ----------
+        t : float
+            unused in LTI model, but required by solve_ivp()
+        state : array of float, shape (5,)
+            5x1 system state with u tacked on at the end:
+            [x, xd, theta, thetad, u]
+
+        Returns
+        -------
+        state : array of float, shape (5,)
+            derivative of the state
+        '''
+        x, xd, theta, thetad, u = state[0], state[1], state[2], state[3], state[4]
+
+        xdd = (u + self.m * self.g * np.sin(theta) * np.cos(theta) \
+            - self.m * self.l * thetad * thetad *  np.sin(theta)) \
+                / (self.M + self.m - self.m * np.cos(theta) * np.cos(theta))
+
+        tdd = (xdd * np.cos(theta) + self.g * np.sin(theta) ) / self.l
+
+        return np.array([xd * (1 - self.cfric), xdd, thetad, tdd, u])
+
+    def solve(self, dt, system_state, u, solve_args={}):
+        '''helper method for solve ivp
+
+        Parameters
+        ----------
+        dt : float
+            [description]
+        system_state : np array of float, shape (4,)
+            system state given by:
+            [x, xd, theta, thetad]
+        u : float
+            scalar force. Positive is ->, negative is <-
+            u is assumed constant throughout the interval dt.
+        solve_args : solver arguments, optional
+            arguments passed to the solver. See https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.solve_ivp.html for
+            more details
+
+        Returns
+        -------
+        tuple of (ndarray, shape (4,), float)
+            the state at the end of dt and the force at the end of dt. 
+            note that force is held constant through the interval.
+        '''
         # roll state and u into a single array
         y = np.empty( (5) )
         y[:4] = system_state
@@ -32,31 +124,38 @@ class Pendulum(object):
         sol = scipy.integrate.solve_ivp(self.system_dynamics, (0, dt), y, **solve_args)
         return sol.y[:4, -1], sol.y[4, -1]
 
-    def get_energy(self, y):
+    def get_energy(self, state):
+        '''Return 
+
+        [extended_summary]
+
+        Parameters
+        ----------
+        state : np array of shape (4,)
+            system state given by [x, xd, theta, thetad]
+
+        Returns
+        -------
+        (float, float float)
+            kinetic energy, potential energy, total energy
+        '''
+        x, xd, theta, thetad = state[0], state[1], state[2], state[3]
         # cart vel^2
-        v_c2 = y[1] * y[1]
+        v_c2 = xd * xd
         # pend vel^2
-        v_p2 = v_c2 - 2 * self.l * y[1] * y[3] * np.cos(y[2]) + self.l * self.l * y[3] * y[3]
+        v_p2 = v_c2 - 2 * self.l * xd * thetad * np.cos(theta) + self.l * self.l * thetad * thetad
+        # kinetic energy
         ke = 0.5 * self.M * v_c2 + 0.5 * self.m * v_p2
-        pe = self.m * self.g * self.l * np.cos(y[2])
+        # potential energy
+        pe = self.m * self.g * self.l * np.cos(theta)
         return ke, pe, ke + pe
 
 class MPC(object):
     def __init__(self, pend, dt, window):
         self.T = window
         self.u_max = 100
-        A = np.array([
-            [0, 1.0, 0, 0],
-            [0, 0, -pend.m * pend.g / pend.M, 0.0],
-            [0, 0, 0, 1.0],
-            [0, 0, pend.g * (pend.m * pend.M) / (pend.l * pend.M), 0.0]
-        ])
-        B = np.array([
-            [0.0],
-            [1.0 / pend.M],
-            [0.0],
-            [-1.0 / (pend.l * pend.M)]
-        ])
+        A = pend.jacA
+        B = pend.jacB
         C, D = np.zeros((1, A.shape[0])), np.zeros((1, 1))
         sys_disc = cont2discrete((A,B,C,D), dt, method='zoh')
         self.A, self.B = sys_disc[0], sys_disc[1]
@@ -73,9 +172,7 @@ class MPC(object):
             constr += [cp.abs(u[0,:]) <= self.u_max]
 
         problem = cp.Problem(cp.Minimize(cost), constr)
-        problem.solve('ECOS', max_iters=50, verbose=True)
-
-
+        problem.solve('ECOS', verbose=True)
         if plot is not None:
             fig, ax, lines = plot
             ax.set_xlim((np.min(list(range(self.T+1))) - 0.5, np.max(list(range(self.T+1))) + 0.1))
@@ -95,7 +192,6 @@ class MPC(object):
                     ydata = np.squeeze(u.value * 0.01)
                     line['linesobj'].set_xdata(xdata)
                     line['linesobj'].set_ydata(ydata)
-
             ax.set_ylim((-3, 3))
             fig.canvas.draw()
             fig.canvas.flush_events()
@@ -112,7 +208,14 @@ class Simulation(object):
         self.t_final = t_final # end at or before this time
         self.force = force # forcing function
     
-    def simulate(self, pendulum, controller):
+    def simulate(self, pendulum, controller, **kwargs):
+
+
+        # unpack kwargs
+        plot = kwargs.pop('plot', False)
+
+
+
         times = []
         t = 0
         state = pendulum.y_0
@@ -125,30 +228,31 @@ class Simulation(object):
         while t <= self.t_final:
             times.append(t)
             t += self.dt
-        plt.ion()
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
 
-        l1, = ax.plot(times, [0] * len(times), ls='-', label='x')
-        l2, = ax.plot(times, [0] * len(times), ls='-', label='t')
-        l3, = ax.plot(times, [0] * len(times), ls='--', label='u')
-        h3 = ax.axhline(y=0, color='k', linestyle=':', label='x (setpoint)')
-        # future estimates
-        lines = [
-            {   'linesobj' : l1,
-                'index' : 0,
-                'type'  : 'plot'},
-            {   'linesobj' : l2,
-                'index' : 2,
-                'type'  : 'plot'},
-            {   'linesobj' : l3,
-                'type'  : 'action',
-                'index' : None},
-            {   'linesobj' : h3,
-                'index' : 0,
-                'type'  : 'hline'}
-        ]
-        ax.legend()
+        if plot:
+            plt.ion()
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            l1, = ax.plot(times, [0] * len(times), ls='-', label='x')
+            l2, = ax.plot(times, [0] * len(times), ls='-', label='t')
+            l3, = ax.plot(times, [0] * len(times), ls='--', label='u')
+            h3 = ax.axhline(y=0, color='k', linestyle=':', label='x (setpoint)')
+            # future estimates
+            lines = [
+                {   'linesobj' : l1,
+                    'index' : 0,
+                    'type'  : 'plot'},
+                {   'linesobj' : l2,
+                    'index' : 2,
+                    'type'  : 'plot'},
+                {   'linesobj' : l3,
+                    'type'  : 'action',
+                    'index' : None},
+                {   'linesobj' : h3,
+                    'index' : 0,
+                    'type'  : 'hline'}
+            ]
+            ax.legend()
         
         for k, t in enumerate(times):
             data = {}
@@ -172,11 +276,35 @@ class Simulation(object):
             data[('forces','forces')] = force
             data[('control action','control action')] = action
             force = action
-            state, _ = pendulum.solve(self.dt, state, force, {})
+            state, _ = pendulum.solve(self.dt, state, force)
             for k, v in data.items():
                 datas[k].append(v)
-        plt.ioff()
+        if plot:
+            plt.ioff()
         return pd.DataFrame(datas, index=times)
+    
+    def simulate_multiple(self, pendulums, controllers, parallel=True):
+        if len(pendulums) != len(controllers):
+            raise ValueError('pendulums and controllers must have same length. len(pendulums)={}, len(controllers)={}'.format(len(pendulums), len(controllers)))
+
+        if parallel:
+            pool = Pool(16)
+            print('Simulating {} runs.'.format(len(pendulums)))
+            tic = datetime.now()
+            results = pool.starmap(self.simulate, zip(pendulums, controllers))
+            toc = datetime.now()
+            print('finished in {}'.format(toc - tic))
+            return pd.concat(results, axis=0, keys=list(range(len(results))))
+        else:
+            print('Simulating {} runs.'.format(len(pendulums)))
+            tic = datetime.now()
+            allresults = []
+            for pendulum, controller in zip(pendulums, controllers):
+                results = self.simulate(pendulum, controller)
+                allresults.append(results)
+            toc = datetime.now()
+            print('finished in {}'.format(toc - tic))
+            return pd.concat(allresults, axis=0, keys = list(range(len(results))))
 
 def array_to_kv(level1_key, level2_keys, array):
     data={}
@@ -206,6 +334,6 @@ if __name__ == '__main__':
         10,
         lambda t: 0
     )
-    results = sim.simulate(pend, cont)
+    results = sim.simulate(pend, cont, plot=True)
     viz = viz.Visualizer(results, pend, frameskip=1, draw_ghost=True)
     viz.display_viz()
